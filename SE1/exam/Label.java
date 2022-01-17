aeed74dcd4aee4cf30b971f10ac9ec1ee1c5aea0
Quan Ha <quanhaminh12@gmail.com>
2022-01-18 07:50:38 +0900

    change

--- SE1/exam/Label.java
+++ SE1/exam/Label.java
@@ -4,10 +4,11 @@ public class Label {
     public static final String HEADER = "--- INIAD ---\n";
     private int id;
     private String name;
+    private static int cnt = 1;
 
     public Label(String name) {
         this.name = name;
-        id++;
+        this.id = cnt++;
     }
 
     public String getDisplay() {
