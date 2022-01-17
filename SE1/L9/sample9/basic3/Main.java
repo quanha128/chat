fec9668a241b0973b2f3be21ebf30c4c0b9508b0
Quan Ha <quanhaminh12@gmail.com>
2022-01-18 07:20:16 +0900

    updates

--- SE1/L9/sample9/basic3/Main.java
+++ SE1/L9/sample9/basic3/Main.java
@@ -1,9 +1,11 @@
 package L9.sample9.basic3;
 
+import java.time.LocalDate;
+
 public class Main{
     public static void main(String[] args) {
-        User user = new User("Enryo", "Inoue");
+        User user = new User("Enryo", "Inoue", LocalDate.now());
 
-        System.out.printf("Name : %s \n", user.getName());
+        System.out.printf("Age : %d \n", user.getAge(LocalDate.of(2023,2,1)));
     }
 }
--- SE1/L9/sample9/basic3/User.java
+++ SE1/L9/sample9/basic3/User.java
@@ -5,13 +5,21 @@ import java.time.LocalDate;
 public class User {
     private String firstName;
     private String familyName;
+    private LocalDate birthday;
+    private static int idCounter = 1;
+    private int id;
 
-    public User(String firstName, String familyName) {
+    public User(String firstName, String familyName, LocalDate birthday) {
         this.firstName = firstName;
         this.familyName = familyName;
+        this.birthday = birthday;
     }
 
     public String getName() {
         return String.format("%s %s", familyName, firstName);
+    }
+
+    public int getAge(LocalDate when) {
+      return birthday.until(when).getYears();
     }
 }
